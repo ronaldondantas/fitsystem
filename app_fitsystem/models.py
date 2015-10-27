6633e4c92d0a8309183bd2f889adee5c64ce0bb5
Ronaldo Dantas <ronaldonerydantasf@gmail.com>
2015-10-27 02:21:28 -0300

    Adicionado views e corrigido date dos models

--- app_fitsystem/models.py
+++ app_fitsystem/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 
 class Student(models.Model):
     name = models.CharField(max_length=50)
-    birth_date = models.DateField()
-    last_payment = models.DateField(
-            default=timezone.now)
-    payment_date = models.DateField(
-            default=timezone.now)
+    birth_date = models.DateField(blank=False, default=timezone.now().strftime("%d.%m.%Y"))
+    last_payment = models.DateField(blank=False, default=timezone.now().strftime("%d.%m.%Y"))
+    payment_date = models.DateTimeField(
+            blank=True, null=True)
+
 
     def pay(self):
         self.last_payment = timezone.now()
--- app_fitsystem/views.py
+++ app_fitsystem/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Student
 
 def student_list(request):
-    return render(request, 'app_fitsystem/student_list.html', {})
+	students = Student.objects.filter(payment_date__lte=timezone.now()).order_by('payment_date')
+ 	return render(request, 'app_fitsystem/student_list.html', {'students' : students})
