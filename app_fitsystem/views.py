18ed670780d1fb974b197216e2be035d07e9e8d4
Ronaldo Dantas <ronaldonerydantasf@gmail.com>
2015-10-27 02:23:33 -0300

    Correção de identação

--- app_fitsystem/views.py
+++ app_fitsystem/views.py
@@ -4,4 +4,4 @@ from .models import Student
 
 def student_list(request):
 	students = Student.objects.filter(payment_date__lte=timezone.now()).order_by('payment_date')
- 	return render(request, 'app_fitsystem/student_list.html', {'students' : students})
+	return render(request, 'app_fitsystem/student_list.html', {'students' : students})
